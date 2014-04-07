17e3482dfad2657bd6ab1cdc4264372c6c7c3e14
Max Goldman <maxg@mit.edu>
2014-04-07 10:56:11 -0400

    Starting code.

---
+++ src/warmup/Main.java
@@ -0,0 +1,15 @@
+package warmup;
+
+/**
+ * TODO: put documentation for your class here
+ */
+public class Main {
+    
+    /**
+     * TODO: describe your main function's command line arguments here
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
