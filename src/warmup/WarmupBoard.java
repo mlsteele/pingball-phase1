af5a168cd3c972e6a6a7f07619d5fd66dd8455e3
Miles Steele <miles@milessteele.com>
2014-04-09 11:14:29 -0400

    implement toString

--- src/warmup/WarmupBoard.java
+++ src/warmup/WarmupBoard.java
@@ -26,6 +26,37 @@ public class WarmupBoard {
 
     @Override
     public String toString() {
-        // print the board
+        // Round ball position to grid square
+        Vect ballpos = ball.getCenter();
+        int ballX = (int) ballpos.x();
+        int ballY = (int) ballpos.y();
+
+        // Create rep string
+        String boardString = "";
+        for (int y = 0; y < width; y++) {
+        for (int x = 0; x < width; x++) {
+            if (ballX == x && ballY == y) {
+                boardString += "*";
+            } else if (isOnBoundary(x,y)) {
+                boardString += ".";
+            } else {
+                boardString += " ";
+            }
+        }
+        }
+        return boardString;
+    }
+
+    /**
+     * Check if a coordinate point is on the outer boundary of the board.
+     * @param  x x coordinate
+     * @param  y y coordinate
+     * @return   boolean indicating whether point is on board boundary.
+     */
+    private boolean isOnBoundary(int x, int y) {
+        return x == 0
+            || x == width -1
+            || y == 0
+            || y == height -1;
     }
 }
