7779e4ce5825a8bbb7815c591d1313805399fc17
annawalsh221 <awalsh28@mit.edu>
2014-04-10 18:17:30 -0400

    Initial Specs

--- src/client/gadgets/StaticBumper.java
+++ src/client/gadgets/StaticBumper.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import physics.LineSegment;
 import physics.Vect;
+import client.BoardEvent;
 
 /**
  * StaticBumper is a stationary geometric bumper.
@@ -17,9 +18,9 @@ public class StaticBumper implements Gadget {
      * Constructor that takes the shape of the bumper.
      */
     private StaticBumper(String name, List<LineSegment> geometry) {
-        
+
     }
-    
+
     /**
      * Factory method for creating SquareBumpers
      */
@@ -27,5 +28,35 @@ public class StaticBumper implements Gadget {
         // TODO
         return new StaticBumper(name, new ArrayList<LineSegment>());
     }
-    
+
+    @Override
+    public BoardEvent handleBall() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public client.gadgets.Vect getPosition() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String stringRepresentation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void specialAction() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
