1b3c24dbb78c2fff5f7dd69378bc71a040e8f253
Jess Kenney <seveneightn9ne@gmail.com>
2014-04-13 16:33:05 -0400

    Start server implementation

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,8 +1,14 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
- * Creates thread to handle client's requests. These include granting fuse/separate board
- * requests and receiving and sending BallIn/Out messages.
+ * Creates thread to handle client's requests. These include ball out messages
+ * and client connect/disconnect
  *
  * Thread safety argument:
  * A queue will receive client requests and process them in order.
@@ -10,11 +16,61 @@ package server;
 
 public class ClientHandler implements Runnable{
 
+    private final Socket socket;
+    private final BufferedReader in;
+    private final PrintWriter out;
+    private boolean running;
+
+    /**
+     * Make a new ClientHandler
+     * @param socket the socket through which we communicate with the client
+     * @throws IOException
+     */
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.running = false;
+    }
+
+    /**
+     * Handles the incoming client messages
+     */
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        running = true;
+        // handle the client
+        try {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                // deserialize line
 
+                // handle it
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            running = false;
+            try {
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    /**
+     * Send a message to the client. Requires the thread to be running!
+     * @param message the message to send to the client
+     */
+    public void send(NetworkMessage message) {
+        if (running) {
+            String strMessage = message.serialize();
+            out.println(strMessage);
+        }
+    }
+
 
 
 }
