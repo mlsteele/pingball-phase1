16ef5da87dab7520147b3eefc951b74ee2669fdc
Miles Steele <miles@milessteele.com>
2014-04-13 22:25:33 -0400

    add client specs

--- src/client/ServerHandler.java
+++ src/client/ServerHandler.java
@@ -7,10 +7,14 @@ import java.net.Socket;
  * It handles communication with the server including wire protocol and non-blockng communication with the socket.
  *
  * Thread Safety Argument:
- * - socket is confined to this thread after construction.
+ * - the socket reference is confined to this thread.
+ *     - reading from the socket is confined to the ServerHandler thread.
+ *     - writing to the socket is confined to the PingballClient thread.
+ * - incomingMessages is a threadsafe datatype.
  */
 public class ServerHandler implements Runnable {
     private final Socket socket;
+    private final BlockingQueue incomingMessages;
 
     /**
      * Create a ServerHandler.
@@ -18,13 +22,27 @@ public class ServerHandler implements Runnable {
      * @param socket to communicate with the server.
      *        socket must already be connected.
      *        the caller must throw away their reference to socket after creating a ServerHandler
+     * @param incomingMessages threadsafe queue to receive messages
      */
-    ServerHandler(Socket socket) {
+    ServerHandler(Socket socket, BlockingQueue incomingMessages) {
         this.socket = socket;
+        this.incomingMessages = incomingMessages;
     }
 
+    /**
+     * Start listening for messages from the server.
+     */
     @Override
     public void run() {
         // TODO listen to and send messages in a loop
+    }
+
+    /**
+     * Send a message to the server. Requires the thread to be running!
+     * @param message message to send
+     */
+    public void send(NetworkMessage message) {
+        String strMessage = message.serialize();
+        out.println(strMessage);
     }
 }
