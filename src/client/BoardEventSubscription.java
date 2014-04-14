65c17a228c795007abd9eedebb00230b4767ea46
Jess Kenney <seveneightn9ne@gmail.com>
2014-04-13 22:59:21 -0400

    Specs for BoardEvent and BoardEventSubscription

--- src/client/BoardEventSubscription.java
+++ src/client/BoardEventSubscription.java
@@ -6,21 +6,28 @@ import client.gadgets.Gadget;
  * A Subscription is an immutable connection between a Gadget that
  * creates BoardEvents and a Gadget that acts on those events.
  *
+ * Subscriptions persist for the duration of the board game, unlike
+ * BoardEvents which are consumed.
+ *
  */
 public class BoardEventSubscription {
     private final Gadget subscriber;
     private final Gadget triggerer;
 
     /**
-     * TODO
-     * @param subscriber
-     * @param triggerer
+     * Create a new BoardEventSubscription
+     * @param subscriber the Gadget that will do something when triggerer fires a BoardEvent
+     * @param triggerer the Gadget that will fire BoardEvents
      */
     public BoardEventSubscription(Gadget subscriber, Gadget triggerer) {
         this.subscriber = subscriber;
         this.triggerer = triggerer;
     }
 
+    /**
+     * get the triggerer
+     * @return the triggerer, i.e. the Gadget whose BoardEvents will cause the subscriber to act.
+     */
     public Gadget getTriggerer() {
         return triggerer;
     }
