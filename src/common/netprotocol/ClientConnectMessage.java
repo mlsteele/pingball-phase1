7f5ed8c5220309f8a90bd06c79a32838e8fd66ff
Jess Kenney <seveneightn9ne@gmail.com>
2014-04-18 18:47:17 -0400

    fixes for ClientConnectMessage

--- src/common/netprotocol/ClientConnectMessage.java
+++ src/common/netprotocol/ClientConnectMessage.java
@@ -1,5 +1,7 @@
 package common.netprotocol;
 
+import common.Constants;
+
 /**
  * Client -> Server
  * Message sent when a client wants to connect to a server.
@@ -7,24 +9,26 @@ package common.netprotocol;
  *
  * ADT:
  * * ClientConnectMessage contains the following data:
- * * * boardName: the name of the board connecting
+ *      * boardName: the name of the board connecting
  *
  * Thread Safety Argument:
- * - all data is immutable.
+ * * all data is immutable.
+ *
+ * Rep Invariant:
+ * * all data is immutable (enforced by type system)
+ *
  */
 public class ClientConnectMessage extends NetworkMessage {
-    /**
-     * Rep invariant:
-     * - all data is immutable (enforced by type system)
-     */
+
     private final String boardName;
 
     /**
      * Deserialize the message.
      * See NetworkMessage.deserialize for specification.
      * See this NetworkMessage's serialize for specific serialization specification.
-     * @return decoded NetworkMessage
+     *
      * @param body body of the message
+     * @return decoded NetworkMessage
      */
     public static NetworkMessage deserialize(String body) throws DecodeException {
         String units[] = body.split(STD_SEP);
@@ -44,6 +48,8 @@ public class ClientConnectMessage extends NetworkMessage {
      */
     public ClientConnectMessage(String boardName) {
         this.boardName = boardName;
+
+        if (Constants.DEBUG) checkRep();
     }
 
     /**
@@ -57,9 +63,18 @@ public class ClientConnectMessage extends NetworkMessage {
     }
 
     /**
-     * @return return boardName
+     * @return boardName
      */
     public String getBoardName() {
         return boardName;
+    }
+
+    /**
+     * Our rep invariant is enforced by Java type-checking.
+     *
+     * This is a do-nothing method.
+     */
+    private void checkRep() {
+        return;
     }
 }
